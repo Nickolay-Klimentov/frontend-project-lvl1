f7e94b28859861120bf8aa166720c2bcce3871a1
Nickolay Klimentov <moody.klmn@icloud.com>
2022-06-05 15:06:29 +0300

    last fix in index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
 import readlineSync from 'readline-sync';
 
 const playGame = (question, generateRoundData) => {
-  // let correctAnswersCount = 0;
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  // const winAnswersCount = 3;
   console.log(question);
-  // while (correctAnswersCount < winAnswersCount) {
   for (let i = 1; i <= 3; i += 1) {
     const [roundQuestion, correctAnswer] = generateRoundData();
     console.log(`Question: ${roundQuestion}`);
     const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-      // correctAnswersCount += 1;
-    } else {
+    if (answer !== correctAnswer) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${name}!`);
 };
